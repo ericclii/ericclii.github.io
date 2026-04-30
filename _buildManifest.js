12221e15b719fafe6cf2d56af5b1b990d69d47a3
Eric <90214643+ericclii@users.noreply.github.com>
2026-04-30 12:16:22 -0400

    Add files via upload

---
+++ _buildManifest.js
@@ -0,0 +1,62 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0ccnjk9de7ib9.js"
+  ],
+  "/404": [
+    "static/chunks/0kw5mlq.~vc2~.js"
+  ],
+  "/[slug]": [
+    "static/chunks/0y5g996ber7h8.js"
+  ],
+  "/_error": [
+    "static/chunks/0ud4v4xe6pdpo.js"
+  ],
+  "/blog": [
+    "static/chunks/0_6v9s0pmi9~5.js"
+  ],
+  "/demos/serverless-discord-oauth": [
+    "static/chunks/18c0qg17nk28y.js"
+  ],
+  "/experiments": [
+    "static/chunks/0lp~3ej2.94l5.js"
+  ],
+  "/experiments/morphing-shapes": [
+    "static/chunks/0_pos0wvol2qm.js"
+  ],
+  "/experiments/rekordbox-history-parser": [
+    "static/chunks/0s5vn_dbpyr2n.js"
+  ],
+  "/monzo/dashboard": [
+    "static/chunks/0shyzp98dozte.js"
+  ],
+  "/stats": [
+    "static/chunks/1275ngpft1ss..js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/404",
+    "/_app",
+    "/_error",
+    "/api/contact",
+    "/api/map",
+    "/api/oauth",
+    "/api/oauth/[platform]/callback",
+    "/api/oauth/[platform]/redirect",
+    "/api/og",
+    "/api/ping",
+    "/api/posts",
+    "/blog",
+    "/demos/serverless-discord-oauth",
+    "/experiments",
+    "/experiments/morphing-shapes",
+    "/experiments/rekordbox-history-parser",
+    "/monzo/dashboard",
+    "/stats",
+    "/[slug]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
